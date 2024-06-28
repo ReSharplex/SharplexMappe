d53d3bd58115e7bcb70bc8c72a151349d32ca905
ReSharplex <166249324+ReSharplex@users.noreply.github.com>
2024-06-28 22:02:05 +0200

    initial LeetCode project

---
+++ LeetCodeExamples/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+
+namespace LeetCodeExamples;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var res = IsMatch("aab", "c*a*b");
+        Console.WriteLine(res);
+    }
+
+    public static bool IsMatch(string s, string p)
+    {
+        if (p == ".*") return true;
+
+        bool isStar = false;
+        char lastChar = ' ';
+        
+        int count = 0;
+        while (count < s.Length)
+        {
+            if (count < p.Length)
+            {
+                var charOfP = p[count];
+                var charOfS = s[count];
+
+                if (charOfP == '*')
+                {
+                    isStar = true;
+                    lastChar = count == 0 ? charOfS : s[count - 1];
+                    count++;
+                    continue;
+                }
+            
+                if (charOfS != charOfP)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                var charOfS = s[count];
+                
+                if (isStar)
+                {
+                    if (lastChar != charOfS)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            count++;
+        }
+
+        if (isStar) return true;
+        return false;
+    }
+}
