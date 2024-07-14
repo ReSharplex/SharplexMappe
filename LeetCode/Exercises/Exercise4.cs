0f4b453f5ddd63cca7122bc1b9e9d85f02ca5d93
ReSharplex <166249324+ReSharplex@users.noreply.github.com>
2024-07-14 10:35:37 +0200

    finished exercise4

--- LeetCode/Exercises/Exercise4.cs
+++ LeetCode/Exercises/Exercise4.cs
@@ -6,15 +6,28 @@ public class Exercise4() : Exercise(4)
 {
     private double FindMedianSortedArrays(int[] nums1, int[] nums2)
     {
+        List<int> nums = new List<int>();
+        nums.AddRange(nums1);
+        nums.AddRange(nums2);
 
-        return 0;
+        nums.Sort();
+
+        int length = nums.Count;
+
+        if (length % 2 == 0)
+        {
+            int split = length / 2;
+            return (nums[split] + nums[split - 1]) / 2.0d;
+        }
+
+        return nums[length / 2];
     }
-    
+
     public override void Print()
     {
         base.Print();
 
-        var res = FindMedianSortedArrays([1, 3], [2]);
+        var res = FindMedianSortedArrays([1, 2], [3, 4]);
         PrinterManager.Print(res);
     }
 }
